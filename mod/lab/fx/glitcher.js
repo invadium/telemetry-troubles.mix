0edb50587de3742698b3cd9e2639b9b2954e1fb1
Igor Khotin <khotin@gmx.com>
2026-04-20 19:56:13 +0300

    [*] configurable glitches

--- mod/lab/fx/glitcher.js
+++ mod/lab/fx/glitcher.js
@@ -1,19 +1,76 @@
+const superGlitchy = {
+    short: {
+        bias:       .1,
+        threshold: .6,
+    },
+    long: {
+        bias:       10,
+        threshold: .48,
+    },
+    shift: {
+        fq:        .7,
+        // short:      0,
+        // long:       0,
+    },
+    screen: {
+        width:  320,
+        height: 200,
+        backgroundColor: [.2, .2, .1, 1],
+        curvature: {
+            x: 3.0,
+            y: 3.0,
+        },
+        scanLineOpacity: {
+            x: .025,
+            y: .2,
+        },
+        vignette: {
+            x: .5,
+            y: .5,
+            z: 25,
+        },
+    },
+}
+
+const glitchy = {
+    short: {
+        bias:       .1,
+        threshold: .45,
+    },
+    long: {
+        bias:       7,
+        threshold: .28,
+    },
+    shift: {
+        fq: .7,
+    },
+    screen: {
+        width:  480,
+        height: 320,
+        backgroundColor: [.07, .15, .12, 1],
+        curvature: {
+            x: 4.0,
+            y: 4.0,
+        },
+        scanLineOpacity: {
+            x: .025,
+            y: .2,
+        },
+        vignette: {
+            x: .5,
+            y: .5,
+            z: 25,
+        },
+        hBending: {
+            r:  .5,
+            g:   0,
+            b: -.5,
+        },
+    },
+}
+
 function init() {
-    extend(this, {
-        short: {
-            bias:       .1,
-            threshold: .6,
-        },
-        long: {
-            bias:       10,
-            threshold: .48,
-        },
-        shift: {
-            fq:        .7,
-            short:      0,
-            long:       0,
-        },
-    })
+    extend(this, glitchy)
 }
 
 function evo(dt) {
@@ -21,6 +78,6 @@ function evo(dt) {
 
     const v = .5 * (lib.ken.pnoise(env.time * shift.fq, 0, 1) + 1)
     shift.val0 = v
-    shift.val1 = clamp(v - (1 - short.threshold), 0, 1) * short.bias
-    shift.val2 = clamp(v - (1 - long.threshold), 0, 1)  * long.bias
+    shift.shortLevel = clamp(v - (1 - short.threshold), 0, 1) * short.bias
+    shift.longLevel  = clamp(v - (1 - long.threshold), 0, 1)  * long.bias
 }
--- mod/lab/gl/screenLayer.spawn.js
+++ mod/lab/gl/screenLayer.spawn.js
@@ -25,28 +25,31 @@ const screenLayer = {
     },
 
     fixCrtUniforms() {
-        const width      = 320,
-              height     = 200,
+        const gfx = lab.fx.glitcher
+
+        const width      = gfx.screen.width,
+              height     = gfx.screen.height,
               fragWidth  = 1/width,
               fragHeight = 1/height,
               hAspect    = width/height,
               vAspect    = height/width
-
         const lx = rx(.5),
               ly = ry(.5)
 
-        const shiftFx = lab.fx.glitcher.shift
-
         // setup crt uniforms
         gl.uniform1f( gl.getUniformLocation(this.glProg, 'time'), env.time )
-        gl.uniform4f( gl.getUniformLocation(this.glProg, 'glitchLevels'), shiftFx.val1, shiftFx.val2, 0, 0 )
-        gl.uniform4f( gl.getUniformLocation(this.glProg, 'backgroundColor'), .2, .2, 0.1, 1.0 )
+        gl.uniform4f( gl.getUniformLocation(this.glProg, 'glitchLevels'), gfx.shift.shortLevel, gfx.shift.longLevel, 0, 0 )
+        const bc = gfx.screen.backgroundColor
+        gl.uniform4f( gl.getUniformLocation(this.glProg, 'backgroundColor'), bc[0], bc[1], bc[2], bc[3] )
         gl.uniform4f( gl.getUniformLocation(this.glProg, 'screenResolution'), width, height, fragWidth, fragHeight )
         gl.uniform2f( gl.getUniformLocation(this.glProg, 'aspectRate'), hAspect, vAspect )
-        gl.uniform2f( gl.getUniformLocation(this.glProg, 'curvature'), 3.0, 3.0 )
-        gl.uniform2f( gl.getUniformLocation(this.glProg, 'scanLineOpacity'), .025, .2 )
-        gl.uniform3f( gl.getUniformLocation(this.glProg, 'vignetteOpt'), .5, .5, 25 )
-        gl.uniform3f( gl.getUniformLocation(this.glProg, 'hBending'), 0.7*fragWidth, 0.0*fragWidth, -0.7*fragWidth )
+        gl.uniform2f( gl.getUniformLocation(this.glProg, 'curvature'), gfx.screen.curvature.x, gfx.screen.curvature.y )
+        const slo = gfx.screen.scanLineOpacity
+        gl.uniform2f( gl.getUniformLocation(this.glProg, 'scanLineOpacity'), slo.x, slo.y )
+        const vg = gfx.screen.vignette
+        gl.uniform3f( gl.getUniformLocation(this.glProg, 'vignetteOpt'), vg.x, vg.y, vg.z )
+        const hb = gfx.screen.hBending
+        gl.uniform3f( gl.getUniformLocation(this.glProg, 'hBending'), hb.r*fragWidth, hb.g*fragWidth, hb.b*fragWidth )
     },
 
     fixUniforms() {
