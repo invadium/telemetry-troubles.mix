422e7b55200d1884d547fb3776ddb9f54428a0aa
Igor Khotin <khotin@gmx.com>
2026-06-22 02:43:55 +0300

    [+] add/sub math experiment

---
+++ mc.mod/exp/s1/e4-math.js
@@ -0,0 +1,16 @@
+const experiment = {
+    title:     'Math Functions Test',
+    reward:     750,
+
+    verify: function(probe, MC) {
+        const sT = probe.dusty.spy.state()
+
+        const addList = sT.capsule.filter(e => e === 'ADD')
+        const subList = sT.capsule.filter(e => e === 'SUB')
+
+        if (addList.length > 0 && subList.length > 0) return true
+        return false
+    }
+}
+
+module.exports = experiment
--- mc.mod/exp/s1/e7-datalines.js
+++ mc.mod/exp/s1/e7-datalines.js
@@ -4,12 +4,17 @@ const experiment = {
 
     // verify that we have achieved the desired effect/state
     verify: function(probe, MC) {
-        log('verifying that the telemetry on datalines #3-7 is open...')
+        log('verifying that the telemetry on datalines #1-7 is open...')
 
-        for (let i = 3; i <= 7; i++) {
+        for (let i = 0; i <= 7; i++) {
             if (!probe.dataLines[i].telemetry) return false
         }
         return true
+    },
+    // next custom actions after the experiment is completed
+    next: function(probe, MC) {
+        const eS = job.control.emailScheduler
+        eS.sendAfter('unlock-all', 10)
     },
 }
 module.exports = experiment
